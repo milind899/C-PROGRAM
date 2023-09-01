27c141fee8a0739e4439f7f81e8deb233e467b15
Milind Shandilya <76685357+milind899@users.noreply.github.com>
2023-09-01 17:19:14 +0000

    int data

--- data_types.c
+++ data_types.c
@@ -2,6 +2,8 @@
 
 int main()
 {
-    printf("Hello world");
+    //Integer Data Type
+    int age = 19; //4 bytes, used to store whole numbers (Range: -2,147,483,648 to 2,147,483,647)
+    printf("Age: %d\n", age);
     return 0;
 }

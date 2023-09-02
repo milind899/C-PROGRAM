c820b701d67b2b8511dd7139c8f0acbf6ed88f8b
Milind Shandilya <76685357+milind899@users.noreply.github.com>
2023-09-02 15:37:37 +0000

    arithemtic_operator

---
+++ arithemtic_operator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    // arithematic operators
+
+    // +(addition)
+    // -(subtraction)
+    // *(multiplication)
+    // /(division)
+    // %(modulus)
+    // ++(increment)
+    // --(decrement)
+
+    int x = 8;
+    int y = 6;
+    float z = x / (float)y; //using (float) to convert y from int to float variable
+
+    printf("%f", z);
+
+    return 0;
+}
